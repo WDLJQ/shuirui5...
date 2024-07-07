31d4039659655d3c6d01aca8c52a64e8bac8eb4d
sunshinedusk <110714527+sunshinedusk@users.noreply.github.com>
2024-07-07 20:23:50 +0800

    增加按类别生成书籍榜单，榜单按收藏数排序

--- backend/ebookCommunity-master/src/main/java/cn/pyk/mapper/BookMapper.java
+++ backend/ebookCommunity-master/src/main/java/cn/pyk/mapper/BookMapper.java
@@ -1,9 +1,15 @@
-package cn.pyk.mapper;
+package main.java.cn.pyk.mapper;
 
 import cn.pyk.entity.Book;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface BookMapper extends BaseMapper<Book> {
+    @Select("SELECT * FROM book WHERE cid = #{cid} ORDER BY collect DESC LIMIT 10")
+    List<Book> selectTop10BooksByCategory(@Param("cid") int cid);
 }
