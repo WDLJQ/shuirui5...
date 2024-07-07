31e51fb744dfd57dce610b9554339901dec4bb49
Zhang-fulai <129665815+Zhang-fulai@users.noreply.github.com>
2024-07-07 20:27:35 +0800

    Update NavMenu.vue

--- front/ebookCommunity-front/src/components/common/NavMenu.vue
+++ front/ebookCommunity-front/src/components/common/NavMenu.vue
@@ -10,8 +10,8 @@
       <el-menu-item v-for="(item,i) in navList" :key="i" :index="item.name">
         {{ item.navItem }}
       </el-menu-item>
-      <a href="#nowhere" style="color: #222;float: right;padding: 20px;">更多功能</a>
-      <i class="el-icon-menu" style="float:right;font-size: 45px;color: #222;padding-top: 8px"></i>
+      <!-- <a href="#nowhere" style="color: #222;float: right;padding: 20px;">更多功能</a>
+      <i class="el-icon-menu" style="float:right;font-size: 45px;color: #222;padding-top: 8px"></i> -->
       <span style="position: absolute;padding-top: 20px;right: 43%;font-size: 20px;font-weight: bold">White Jotter - Your Mind Palace</span>
     </el-menu>
 </template>
@@ -25,8 +25,7 @@
           {name: '/index', navItem: '首页'},
           {name: '/jotter', navItem: '笔记本'},
           {name: '/library', navItem: '图书馆'},
-          {name: '/personal', navItem: '个人中心'},
-          {name: '/bookorder', navItem: '排行榜'}
+          {name: '/personal', navItem: '个人中心'}
         ]
       }
     },
